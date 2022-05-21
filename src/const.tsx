56c77160a0cf414f669f5e7a2116d75fbd3090b3
tharoonb <89992077+tharoonb@users.noreply.github.com>
2022-05-21 13:46:09 -0400

    rpc and address

--- src/const.tsx
+++ src/const.tsx
@@ -2,10 +2,10 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export const provider = new WalletConnectProvider({
   rpc: {
-    80001: "https://rpc-mumbai.maticvigil.com/",
+    80001: "https://matic-mumbai.chainstacklabs.com",
   },
   clientMeta: {
-    name: "meshaApp",
+    name: "katan",
     description: "Biggest airdrop is here! World ID example app.",
     url: "https://github.com/worldcoin/world-id-example-airdrop-dapp",
     icons: [
@@ -16,7 +16,7 @@ export const provider = new WalletConnectProvider({
 
 export const CONTRACT_ADDRESS =
   process.env.WLD_CONTRACT_ADDRESS || // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing
-  "0x32D59776E91fdb3F377755e12cEC05d9067c2B4F";
+  "0x6F1168A67e3E8c23A2BbF9444239fcA2431eEbD8";
 
 export const CONTRACT_ABI = [
   // Function to claim the airdrop
